f68905761ddd918dc87ed931a43d82eb00cd8aaf
Aviral Raj <aviralraj20142222@gmail.com>
2022-05-28 13:16:08 +0530

    Adding all the files

---
+++ jarvis.py
@@ -0,0 +1,17 @@
+from json.tool import main
+from threading import main_thread
+import pyttsx3
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+print(voices[1].id)
+engine.setProperty("voice", voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+if __name__ == "main":
+    speak("aviral is a good guy")
